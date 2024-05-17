a7c815da86fe280e8eb95bf72eccf07199b47a59
SamB-12 <bedre.samarth@gmail.com>
2024-05-17 22:18:05 +0530

    check for is location enabled

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,6 +46,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.location)
+    implementation(libs.dexter)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/samsweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/samsweatherapp/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.samsweatherapp
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.samsweatherapp.databinding.ActivityMainBinding
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,5 +29,53 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding?.root)
+
+        if (!isLocationEnabled()){
+            Toast.makeText(this,"Turn on the GPS",Toast.LENGTH_SHORT).show()
+
+            val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+            startActivity(intent)
+        } else {
+            //Toast.makeText(this,"Location is already on!",Toast.LENGTH_SHORT).show()
+
+            Dexter.withContext(this)
+                .withPermissions(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ).withListener(object : MultiplePermissionsListener{
+                    override fun onPermissionsChecked(report: MultiplePermissionsReport?) {
+                        if (report!!.areAllPermissionsGranted()){
+                            Toast.makeText(this@MainActivity,"All Permissions Granted! Go Ahead",Toast.LENGTH_SHORT).show()
+                            //TODO add showRationaleForDialog
+                        }
+
+                        if (report.isAnyPermissionPermanentlyDenied){
+                            Toast.makeText(this@MainActivity,"Location Permissions are denied. Go to settings to allow them",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                    override fun onPermissionRationaleShouldBeShown(
+                        p0: MutableList<PermissionRequest>?,
+                        p1: PermissionToken?
+                    ) {
+                        TODO("Not yet implemented")
+                    }
+
+                })
+        }
+    }
+
+    /**
+     * This method checks if the location service in the device has been turned on.
+     *
+     * return Boolean   A Boolean value that denotes if the location service has been turned on or not
+     */
+    private fun isLocationEnabled():Boolean{
+        val locationManager :LocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    private fun showRationaleForDialog(){
+
     }
 }
