a786e7cefe66b3eda243dcae92e7ba19d39a7938
SamB-12 <bedre.samarth@gmail.com>
2024-05-17 13:16:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/samsweatherapp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.samsweatherapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.samsweatherapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var binding :ActivityMainBinding ?= null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+    }
+}
